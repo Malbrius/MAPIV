e086719a030b76e0d9b9fed5d26440f2f2f704f5
Malbrius <62076819+Malbrius@users.noreply.github.com>
2023-09-11 16:47:33 -0700

    Add files via upload

---
+++ MAPIV - Mesa de Ayuda/orders.js
@@ -0,0 +1,56 @@
+const Orders = [
+    {
+        productName: 'Actualizar Graficas',
+        productNumber: '000122',
+        paymentStatus: 'Rafita',
+        status: 'Esperando Respuesta'
+    },
+    {
+        productName: 'Falla Genesys',
+        productNumber: '000123',
+        paymentStatus: 'Palomitas',
+        status: 'Rechazado'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+]
