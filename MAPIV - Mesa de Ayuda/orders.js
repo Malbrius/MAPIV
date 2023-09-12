9468eaad84b2f05e97f3c4ad7bb0e45b67b50fbf
Malbrius <malbrius@gmail.com>
2023-09-11 18:26:04 -0700

    add
    scroll bar for dashboard and notification

--- MAPIV - Mesa de Ayuda/orders.js
+++ MAPIV - Mesa de Ayuda/orders.js
@@ -53,4 +53,112 @@ const Orders = [
         paymentStatus: 'Rafota',
         status: 'En espera'
     },
+    {
+        productName: 'Actualizar Graficas',
+        productNumber: '000122',
+        paymentStatus: 'Rafita',
+        status: 'Esperando Respuesta'
+    },
+    {
+        productName: 'Falla Genesys',
+        productNumber: '000123',
+        paymentStatus: 'Palomitas',
+        status: 'Rechazado'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'Actualizar Graficas',
+        productNumber: '000122',
+        paymentStatus: 'Rafita',
+        status: 'Esperando Respuesta'
+    },
+    {
+        productName: 'Falla Genesys',
+        productNumber: '000123',
+        paymentStatus: 'Palomitas',
+        status: 'Rechazado'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
+    {
+        productName: 'No agarra el checador',
+        productNumber: '000124',
+        paymentStatus: 'Rafota',
+        status: 'En espera'
+    },
 ]
